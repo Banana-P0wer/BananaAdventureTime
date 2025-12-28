229c1beed9dec261a2e8f79b6a940ceed4b4a8e3
B4n4n4_P0wer <vladtv2374@yandex.ru>
2025-12-28 13:00:48 +0300

    Initial commit

---
+++ src/main/java/net/bananap0wer/bananaadventuretime/BananaAdventureTimeClient.java
@@ -0,0 +1,10 @@
+package net.bananap0wer.bananaadventuretime;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BananaAdventureTimeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bananap0wer/bananaadventuretime/BananaAdventureTimeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bananap0wer.bananaadventuretime;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BananaAdventureTimeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
