6fcf85e410c8151713d73d5f5ded4037799d3956
Vlad <vladtv2374@yandex.ru>
2026-02-02 13:19:48 +0300

    Change ruby ore

--- src/main/java/net/bananap0wer/bananaadventuretime/block/ModBlocks.java
+++ src/main/java/net/bananap0wer/bananaadventuretime/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
@@ -14,19 +15,23 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
     //Add blocks here 1/2
-    public static final Block RUBY_BLOCK = registerBlock("ruby_block", new Block( 
+    public static final Block RUBY_BLOCK = registerBlock("ruby_block", 
+        new Block( 
             AbstractBlock.Settings.create().mapColor(MapColor.DARK_RED).instrument(NoteBlockInstrument.BIT).requiresTool().strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL)
     ));
 
-    public static final Block RUBY_ORE = registerBlock("ruby_ore", new Block( 
-            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+    public static final Block RUBY_ORE = registerBlock("ruby_ore", 
+        new ExperienceDroppingBlock(UniformIntProvider.create(3, 7),
+            AbstractBlock.Settings.copy(Blocks.EMERALD_ORE)
     ));
-    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore", new Block( 
-            AbstractBlock.Settings.copy(RUBY_ORE).mapColor(MapColor.DEEPSLATE_GRAY).strength(4.5F, 3.0F).sounds(BlockSoundGroup.DEEPSLATE)
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore", 
+        new ExperienceDroppingBlock(UniformIntProvider.create(3, 7),
+            AbstractBlock.Settings.copy(Blocks.DEEPSLATE_EMERALD_ORE)
     ));
 
 
