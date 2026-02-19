e25d528823778bdd629c61ecfc950daf99795b18
B4n4n4_P0wer <vladtv2374@yandex.ru>
2026-02-19 13:00:00 +0300

    Berry juice like honey now

--- src/main/java/net/bananap0wer/bananaadventuretime/item/custom/SweetBerriesJuiceItem.java
+++ src/main/java/net/bananap0wer/bananaadventuretime/item/custom/SweetBerriesJuiceItem.java
@@ -1,11 +1,16 @@
 package net.bananap0wer.bananaadventuretime.item.custom;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
 
 public class SweetBerriesJuiceItem extends Item {
     private static final int MAX_USE_TIME = 40;
@@ -17,6 +22,11 @@ public class SweetBerriesJuiceItem extends Item {
     @Override
     public int getMaxUseTime(ItemStack stack, LivingEntity user) {
         return MAX_USE_TIME;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        return ItemUsage.consumeHeldItem(world, user, hand);
     }
 
     @Override
