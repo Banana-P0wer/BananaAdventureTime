9816b72238c4ab3c8fecb670159db6888be8029b
B4n4n4_P0wer <vladtv2374@yandex.ru>
2026-02-24 14:00:00 +0300

    Match ruby ore to emerald rarity

--- src/main/java/net/bananap0wer/bananaadventuretime/world/gen/ModOreGeneration.java
+++ src/main/java/net/bananap0wer/bananaadventuretime/world/gen/ModOreGeneration.java
@@ -3,11 +3,12 @@ package net.bananap0wer.bananaadventuretime.world.gen;
 import net.bananap0wer.bananaadventuretime.world.ModPlacedFeatures;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.world.gen.GenerationStep;
 
 public class ModOreGeneration {
     public static void generateOres() {
-        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES,
-            ModPlacedFeatures.RUBY_ORE_PLACED_KEY);
+        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_MOUNTAIN),
+            GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.RUBY_ORE_PLACED_KEY);
     }
 }
