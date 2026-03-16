5aaf0204311b3f7d368d97402e1640fbe38d1860
B4n4n4_P0wer <vladtv2374@yandex.ru>
2026-03-16 09:00:00 +0300

    Add demon sword recoil damage

--- src/main/java/net/bananap0wer/bananaadventuretime/item/custom/DemonBloodSwordItem.java
+++ src/main/java/net/bananap0wer/bananaadventuretime/item/custom/DemonBloodSwordItem.java
@@ -8,16 +8,25 @@ import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 
 public class DemonBloodSwordItem extends SwordItem {
+    private static final float RECOIL_DAMAGE = 2.0f;
+
     public DemonBloodSwordItem(ToolMaterial toolMaterial, Settings settings) {
         super(toolMaterial, settings);
     }
 
     @Override
     public void postDamageEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        damageAttacker(attacker);
         stack.damage(1, attacker, EquipmentSlot.MAINHAND);
 
         if (stack.isEmpty()) {
             attacker.equipStack(EquipmentSlot.MAINHAND, new ItemStack(ModItems.EMPTY_DEMON_BLOOD_SWORD));
+        }
+    }
+
+    private static void damageAttacker(LivingEntity attacker) {
+        if (!attacker.getWorld().isClient()) {
+            attacker.damage(attacker.getDamageSources().magic(), RECOIL_DAMAGE);
         }
     }
 }
