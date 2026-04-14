efe6c161971ad2d4cb9cdc04a587729cc0815ba2
B4n4n4_P0wer <vladtv2374@yandex.ru>
2026-04-14 13:00:00 +0300

    Require grape sword break for hit advancement

--- src/main/java/net/bananap0wer/bananaadventuretime/item/custom/GrapeSwordItem.java
+++ src/main/java/net/bananap0wer/bananaadventuretime/item/custom/GrapeSwordItem.java
@@ -1,6 +1,8 @@
 package net.bananap0wer.bananaadventuretime.item.custom;
 
+import net.bananap0wer.bananaadventuretime.BananaAdventureTime;
 import net.bananap0wer.bananaadventuretime.item.ModItems;
+import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.boss.WitherEntity;
@@ -8,8 +10,13 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
 
 public class GrapeSwordItem extends SwordItem {
+    private static final Identifier ONE_GOOD_HIT_ID = Identifier.of(BananaAdventureTime.MOD_ID, "combat/one_good_hit");
+    private static final String ONE_GOOD_HIT_CRITERION = "hit_and_broke_grape_sword";
+
     public GrapeSwordItem(ToolMaterial toolMaterial, Settings settings) {
         super(toolMaterial, settings);
     }
@@ -24,6 +31,18 @@ public class GrapeSwordItem extends SwordItem {
 
         if (!(attacker instanceof PlayerEntity player && player.isCreative())) {
             attacker.equipStack(EquipmentSlot.MAINHAND, new ItemStack(ModItems.EMPTY_DEMON_BLOOD_SWORD));
+            grantOneGoodHit(attacker);
+        }
+    }
+
+    private static void grantOneGoodHit(LivingEntity attacker) {
+        if (!(attacker instanceof ServerPlayerEntity player)) {
+            return;
+        }
+
+        AdvancementEntry advancement = player.getServer().getAdvancementLoader().get(ONE_GOOD_HIT_ID);
+        if (advancement != null) {
+            player.getAdvancementTracker().grantCriterion(advancement, ONE_GOOD_HIT_CRITERION);
         }
     }
 }
