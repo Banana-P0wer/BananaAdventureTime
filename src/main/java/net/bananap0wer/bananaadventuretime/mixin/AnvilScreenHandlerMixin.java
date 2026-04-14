4e698d89d4a2b6db3eb38df3f5a2a612b79e2c67
B4n4n4_P0wer <vladtv2374@yandex.ru>
2026-04-14 12:00:00 +0300

    Fix root sword anvil repair mixin

--- src/main/java/net/bananap0wer/bananaadventuretime/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/net/bananap0wer/bananaadventuretime/mixin/AnvilScreenHandlerMixin.java
@@ -6,15 +6,14 @@ import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.screen.AnvilScreenHandler;
-import net.minecraft.screen.ForgingScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ForgingScreenHandler.class)
-public class ForgingScreenHandlerMixin {
+@Mixin(AnvilScreenHandler.class)
+public class AnvilScreenHandlerMixin {
     private static final int INPUT_SLOT = 0;
     private static final int REPAIR_SLOT = 1;
     private static final int OUTPUT_SLOT = 0;
@@ -23,10 +22,6 @@ public class ForgingScreenHandlerMixin {
 
     @Inject(method = "updateResult", at = @At("HEAD"), cancellable = true)
     private void bananaadventuretime$repairRootSwordWithSaplings(CallbackInfo info) {
-        if (!((Object) this instanceof AnvilScreenHandler)) {
-            return;
-        }
-
         Inventory input = ((ForgingScreenHandlerAccessor) this).bananaadventuretime$getInput();
         ItemStack rootSword = input.getStack(INPUT_SLOT);
         ItemStack repairStack = input.getStack(REPAIR_SLOT);
