0dd53dc731cf6db6cbe7eda2a2e250a0d8cc31b6
B4n4n4_P0wer <vladtv2374@yandex.ru>
2026-04-21 12:00:00 +0300

    Make special loot rare

--- src/main/java/net/bananap0wer/bananaadventuretime/util/ModLootTableModifiers.java
+++ src/main/java/net/bananap0wer/bananaadventuretime/util/ModLootTableModifiers.java
@@ -2,13 +2,18 @@ package net.bananap0wer.bananaadventuretime.util;
 
 import net.bananap0wer.bananaadventuretime.item.ModItems;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.item.Item;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.LootTables;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 
 public class ModLootTableModifiers {
+    private static final float EMPTY_ICE_KING_CROWN_CHANCE = 0.15f;
+    private static final float DEMON_BLOOD_SWORD_CHANCE = 0.05f;
+
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
             if (!source.isBuiltin()) {
@@ -30,17 +35,18 @@ public class ModLootTableModifiers {
     }
 
     private static void addEmptyIceKingCrownPool(LootTable.Builder tableBuilder) {
-        LootPool.Builder poolBuilder = LootPool.builder()
-                .rolls(ConstantLootNumberProvider.create(1))
-                .with(ItemEntry.builder(ModItems.EMPTY_ICE_KING_CROWN));
-
-        tableBuilder.pool(poolBuilder.build());
+        addRareItemPool(tableBuilder, ModItems.EMPTY_ICE_KING_CROWN, EMPTY_ICE_KING_CROWN_CHANCE);
     }
 
     private static void addDemonBloodSwordPool(LootTable.Builder tableBuilder) {
+        addRareItemPool(tableBuilder, ModItems.DEMON_BLOOD_SWORD, DEMON_BLOOD_SWORD_CHANCE);
+    }
+
+    private static void addRareItemPool(LootTable.Builder tableBuilder, Item item, float chance) {
         LootPool.Builder poolBuilder = LootPool.builder()
                 .rolls(ConstantLootNumberProvider.create(1))
-                .with(ItemEntry.builder(ModItems.DEMON_BLOOD_SWORD));
+                .conditionally(RandomChanceLootCondition.builder(chance))
+                .with(ItemEntry.builder(item));
 
         tableBuilder.pool(poolBuilder.build());
     }
